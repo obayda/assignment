89fd43eee37e02b4c3290e5ab0688b649ac534c1
obaydah <obaydah@moonsite.co.il>
2020-01-09 11:48:39 +0200

    ready

--- src/containers/webscoket/index.js
+++ src/containers/webscoket/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
 class Sockets extends Component {
-    state = {}
+    state = {
+       data: []
+    }
 
     ws = new WebSocket('wss://ws.bitstamp.net/')
 
-    async componentDidMount() {
+    componentDidMount() {
         const params = {
             'event': 'bts:subscribe',
             'data': {
@@ -18,9 +20,21 @@ class Sockets extends Component {
             console.log('connected')
         }
         this.ws.onmessage = (msg) => {
+            let bids, asks;
+            let message = JSON.parse(msg.data)
+            if (Object.keys(message.data).length != 0) {
+                let length = message.data.bids.length
+                bids = message.data.bids[length-1][0]
+                asks = message.data.asks[length-1][0]
+            }
+            if(this.state.data.length < 10){
+            if(bids && asks){
+                let arr = this.state.data.concat((Math.round(((+bids + +asks)/2) * 100) / 100).toFixed(2))
             this.setState({
-                data: JSON.parse(msg.data).data
+                data: arr
             })
+        }
+    }
         }
         this.ws.onclose = () => {
             console.log('disconnected')
@@ -31,26 +45,23 @@ class Sockets extends Component {
         this.ws.close()
     }
 
-    handleData = (data) => {
-
-    }
-
     render() {
-        console.log('rerender');
         const { data } = this.state;
-        if (data) {
-            if (Object.keys(data).length != 0) {
-                this.handleData((+data.bids[data.bids.length - 1][0] + +data.asks[data.asks.length - 1][0]) / 2);
-            }
-        } else {
-            return <div>Lodaing...</div>
+        if(!data){
+            return <div>Loading...</div>
         }
         return (
             <div>
                 <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-                    <div style={{ marginTop: 80, height: 50, width: '96%', border: 'solid' }} />
+                    <div style={{ marginTop: 80, height: 50, width: '96%', border: 'solid' }}>
+                        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+                            {data.map((e, i)=>{
+                                    if (i==0) return <p key={i}>{e}</p>
+                                    return <p key={i} style={{marginRight: 10, marginLeft: 10, color: (data[i-1] > e) ? 'red' : 'green'}}>{e}</p>
+                            })}
+                        </div>
+                    </div>
                     <div onClick={() => this.onBtnClick()} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: 80, width: 120, height: 40, border: 'solid' }}>Stop</div>
-
                 </div>
             </div>
         );
